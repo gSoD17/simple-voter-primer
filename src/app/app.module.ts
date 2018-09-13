8a3662f64875aa711978d0c24d42b8850596a7e2
Gino17 <galentro451@gmail.com>
2018-09-13 16:18:58 -0700

    added new provider to fetch local mock json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { DataFetcherProvider } from '../providers/data-fetcher/data-fetcher';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataFetcherProvider
   ]
 })
 export class AppModule {}
