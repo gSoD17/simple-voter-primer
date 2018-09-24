795efdb62d025787ddd10d2f51f59704276659ef
Gino17 <galentro451@gmail.com>
2018-09-24 15:01:16 -0700

    addd election-seats page to tabapage, removed instructional text from generated election-seats page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -9,6 +9,7 @@ import { IonicPage } from 'ionic-angular';
         <ion-tabs>
             <ion-tab [root]="tab1Root" tabTitle="Home" tabIcon="md-home"></ion-tab>
             <ion-tab [root]="tab2Root" tabTitle="Parties" tabIcon="md-megaphone"></ion-tab>
+            <ion-tab [root]="tab3Root" tabTitle="Seat Share" tabIcon="md-pie"></ion-tab>
         </ion-tabs>
     `
 })
@@ -17,5 +18,6 @@ export class TabsPage {
 
     tab1Root = 'HomePage';
     tab2Root = 'PartiesPage';
+    tab3Root = 'ElectionSeatsPage';
 
 }
