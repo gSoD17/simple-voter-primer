20fae7728dfef66d260d36f9b6fa31b7e6911f83
Gino17 <galentro451@gmail.com>
2018-09-27 15:04:46 -0700

    moved ng2 chart import from election-seats.module.ts to election-seats-charts.module.ts

--- src/pages/election-seats/election-seats.module.ts
+++ src/pages/election-seats/election-seats.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ElectionSeatsPage } from './election-seats';
-import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     ElectionSeatsPage,
   ],
   imports: [
-    IonicPageModule.forChild(ElectionSeatsPage),
-    ChartsModule
+    IonicPageModule.forChild(ElectionSeatsPage)
   ],
 })
 export class ElectionSeatsPageModule {}
--- src/pages/election-seats/election-seats.ts
+++ src/pages/election-seats/election-seats.ts
@@ -56,6 +56,10 @@ export class ElectionSeatsPage {
 
   }
 
+  gotoChartsPage(data) {
+    this.navCtrl.push('ElectionSeatsChartsPage', { dataLink: data })
+  }
+
   // ionViewWillEnter() {
 
   //   let seatsData = [];
