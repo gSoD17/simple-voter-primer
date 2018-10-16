9ad01b5d003e356774e96817eebbc356ab933846
Gino17 <galentro451@gmail.com>
2018-10-15 20:43:09 -0700

    modified chart to display appropriate axes and labels and changed the response labels

--- src/pages/quiz-result-chart-modal/quiz-result-chart-modal.ts
+++ src/pages/quiz-result-chart-modal/quiz-result-chart-modal.ts
@@ -32,21 +32,76 @@ export class QuizResultChartModalPage {
       type: 'scatter',
       data: {
         datasets: [
-          { label: 'Your Results', data: [this.quizResults] }
+          { label: 'You', data: [this.quizResults], pointRadius: 5, pointHoverRadius: 5, backgroundColor: '#FF69B4' },
+          { label: "Green Party", data: [{x: -15, y: -28}], pointRadius: 5, pointHoverRadius: 5, backgroundColor: '#80aa4e' },
+          { label: "Centre Party", data: [{x: 17, y: -7}], pointRadius: 5, pointHoverRadius: 5, backgroundColor: '#39944a' }
         ]
       },
       options: {
         responsive: true,
+        legend: {
+          position: 'bottom'
+        },
         scales: {
           xAxes: [{
+            id: 'Authoritarian',
+            position: 'top',
+            scaleLabel: {
+              display: true,
+              labelString: 'Authoritarian'
+            },
+            gridLines: {
+              color: '#ffffff'
+            },
             ticks: {
               display: false,
               beginAtZero: true,
               min: -28,
               max: 28
             }
+          }, {
+            id: 'Libertarian',
+            position: 'bottom',
+            scaleLabel: {
+              display: true,
+              labelString: 'Libertarian'
+            },
+            gridLines: {
+              color: '#ffffff'
+            },
+            ticks: {
+              display: false,
+              beginAtZero: true,
+              min: -28,
+              max: 38
+            }
           }],
           yAxes: [{
+            id: 'Left-Wing',
+            position: 'left',
+            scaleLabel: {
+              display: true,
+              labelString: 'Left-Wing'
+            },
+            gridLines: {
+              color: '#ffffff'
+            },
+            ticks: {
+              display: false,
+              beginAtZero: true,
+              min: -28,
+              max: 28
+            }
+          }, {
+            id: 'Right-Wing',
+            position: 'right',
+            scaleLabel: {
+              display: true,
+              labelString: 'Right-Wing'
+            },
+            gridLines: {
+              color: '#ffffff'
+            },
             ticks: {
               display: false,
               beginAtZero: true,
